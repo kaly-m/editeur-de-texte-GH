eea86f9353e3e22188134bfec6764e6f9c448ff8
michielk <kelian.michiel@etu.umontpellier.fr>
2024-09-03 18:02:45 +0200

    fonction remplacer
    CommandeRemplacer fonctionnelle et optimale
    
    fonction remplacer

---
+++ src/main/java/fr/iut/editeur/commande/invoker/CommandeRemplacer.java
@@ -0,0 +1,34 @@
+package fr.iut.editeur.commande.invoker;
+
+import fr.iut.editeur.commande.CommandeDocument;
+import fr.iut.editeur.document.Document;
+
+
+public class CommandeRemplacer extends CommandeDocument {
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer()
+    {
+        if(parameters.length < 3)
+        {
+            System.err.println("Format attendu : remplacer;debut;fin;chaine");
+            return;
+        }
+        int debut = Integer.parseInt(parameters[1]);
+        int fin = Integer.parseInt(parameters[2]);
+
+        String chaine = "";
+        try{
+            chaine = parameters[3];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("la chaine vide a remplacé l'interval");
+        } // traite le cas ou la chaine est vide
+
+        this.document.remplacer(debut,fin,chaine);
+        super.executer();
+    }
+}
